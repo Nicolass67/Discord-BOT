e78a65b77f077ecd11f151a28bcc9c200e5f14dd
Nicolas Rousseau <nicolas.rousseau@epita.fr>
2022-09-21 21:51:53 +0200

    push

--- src/main.py
+++ src/main.py
@@ -1,5 +1,8 @@
+from http import client
 from discord.ext import commands
 import discord
+import random
+from discord.utils import get
 
 intents = discord.Intents.default()
 intents.members = True
@@ -10,7 +13,9 @@ bot = commands.Bot(
     intents = intents # Set up basic permissions
 )
 
-bot.author_id = 0000000  # Change to your discord id
+bot.author_id = 1022113011403604111  # Change to your discord id
+
+intents.presences = True
 
 @bot.event
 async def on_ready():  # When the bot is ready
@@ -21,5 +26,49 @@ async def on_ready():  # When the bot is ready
 async def pong(ctx):
     await ctx.send('pong')
 
-token = "<MY_TOKEN>"
+@bot.command()
+async def name(ctx):
+    await ctx.send(ctx.author.name)
+
+@bot.command()
+async def d6(ctx):
+    await ctx.send(random.randint(1,6))
+    
+@bot.listen("on_message")
+async def on_message(message):
+    if message.content == "Salut tout le monde":
+            await message.channel.send('Salut tout seul ' + message.author.mention)
+            
+
+@bot.command(pass_context=True)
+async def admin(ctx, user : discord.Member):    
+    role = discord.utils.get(ctx.guild.roles, name="Admin")
+    if role == None:
+        guild = ctx.guild
+        await guild.create_role(name="Admin")
+    await user.add_roles(role)
+
+@bot.command()
+async def ban(ctx, member : discord.Member, *, reason = None):
+    await member.ban(reason = reason)
+    
+
+@bot.command()
+async def count(ctx):
+    List = []
+    for member in ctx.guild.members:
+        List.append(str(member.status))
+        print(str(member.status))
+        print(List)
+    
+    idle = List.count("idle")
+    offline = List.count("offline")
+    online = List.count("online")
+    dnd = List.count("dnd")
+    message = str(idle) + " member(s) are idle, " + str(offline) + " member(s) are offline, " + str(online) + " member(s) are online and " + str(dnd) + " members are in Do not Disturb."
+    await ctx.send(message)
+            
+    
+    
+token = "MTAyMjExMjgzOTM0OTA0MzIzMA.GO4lxS.lHA7mkCDCkrgYlVwyX5EzjXg_t8Ak70mVxYihc"
 bot.run(token)  # Starts the bot
